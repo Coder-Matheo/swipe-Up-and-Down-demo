49e3f32ea9d0b9775fc0e72b65804078b2bbdb3c
Coder-Matheo <ali.dinarvand1370@gmail.com>
2021-10-07 18:55:17 +0200

    Init Swipe Up And Down

---
+++ app/src/main/java/rob/myappcompany/leftandrightswipedemo/MainActivity.java
@@ -0,0 +1,44 @@
+package rob.myappcompany.leftandrightswipedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class MainActivity extends AppCompatActivity {
+
+    float x1, x2, y1, y2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent touchEvent) {
+        switch (touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+
+                if (x1 < x2){
+                    Intent i = new Intent(MainActivity.this, SwipeLeft.class);
+                    startActivity(i);
+                }else if (x1 > x2){
+                    Intent i = new Intent(MainActivity.this, SwipeRight.class);
+                    startActivity(i);
+                }
+
+        }
+
+
+        return super.onTouchEvent(touchEvent);
+
+    }
+}
